1055c6c34a91c6c7ccfbfe14f356c4895366c2a1
Mike Dilger <mike@optcomp.nz>
2018-02-21 13:41:33 +1300

    Dependencies we expect

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
+
+#![recursion_limit = "1024"]
+
+// serialization
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+
+// errors
+#[macro_use]
+extern crate error_chain;
+
+// logging
+#[macro_use]
+extern crate log;
+
+// graphics
+extern crate vks;
+extern crate dacite;
+extern crate dacite_winit;
+extern crate winit;
+extern crate siege_mesh;
+
+// files
+extern crate ddsfile;
+extern crate zstd;
+
+// win32
+#[cfg(windows)] extern crate user32;
+#[cfg(windows)] extern crate winapi;
+
+// math
+extern crate siege_math;
+
+// time
+extern crate chrono;
+
+
 #[cfg(test)]
 mod tests {
     #[test]
