cf7c8cfc744989fd722e87ff878cf21f64357ac9
Mike Dilger <mike@optcomp.nz>
2018-03-15 15:13:22 +1300

    Use ggx specular func from filmic worlds (not sure this is right, looks a bit weird)

--- src/renderer/shade.rs
+++ src/renderer/shade.rs
@@ -373,28 +373,34 @@ vec4 level(vec4 irrad) {
   return vec4(irrad.xyz / params.white_point, irrad.a);
 }
 
-vec3 shadingSpecularGGX(vec3 N, vec3 V, vec3 L, float roughness, vec3 F0)
-{
+float G1V(float dotNV, float k) {
+  return 1.0 /  mix(dotNV, 1.0, k);
+}
+
+vec3 shadingSpecularGGX(vec3 N, vec3 V, vec3 L, float roughness, vec3 F0) {
+    float alpha = roughness * roughness;
+
     vec3 H = normalize(V + L);
-    float dotLH = max(dot(L, H), 0.0);
-    float dotNH = max(dot(N, H), 0.0);
+
     float dotNL = max(dot(N, L), 0.0);
     float dotNV = max(dot(N, V), 0.0);
-    float alpha = roughness * roughness;
+    float dotNH = max(dot(N, H), 0.0);
+    float dotLH = max(dot(L, H), 0.0);
 
     // D (GGX normal distribution, Trowbridge-Reitz)
     float alphaSqr = alpha * alpha;
     float denom = dotNH * dotNH * (alphaSqr - 1.0) + 1.0;
     float D = alphaSqr / (pi * denom * denom);
-    // F (Fresnel term)
-    float F_a = 1.0;
-    float F_b = pow(1.0 - dotLH, 5); // manually?
-    vec3 F = mix(vec3(F_b), vec3(F_a), F0);
-    // G (remapped hotness, see Unreal Shading)
-    float k = (alpha + 2 * roughness + 1) / 8.0;
-    float G = dotNL / (mix(dotNL, 1, k) * mix(dotNV, 1, k));
 
-    return D * F * G / 4.0;
+    // F (Fresnel term)
+    float dotLH5 = pow(1.0 - dotLH, 5);
+    vec3 F = mix(vec3(dotLH5), vec3(1.0), F0);
+
+    // G
+    float k = alpha / 2;
+    float G = G1V(dotNL,k) * G1V(dotNV,k);
+
+    return dotNL * D * F * G;
 }
 
 vec4 decode_normal(vec4 n) {
