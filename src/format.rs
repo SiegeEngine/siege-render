05b32e9d12f51169a9e1ef035b3660eb1ebc6af7
Mike Dilger <mike@optcomp.nz>
2018-04-05 19:23:54 +1200

    comment out another info message

--- src/format.rs
+++ src/format.rs
@@ -4,7 +4,7 @@ use dacite::core::{Format, ComponentMapping, ComponentSwizzle};
 
 pub fn from_d3d(format: D3DFormat) -> Option<(Format, ComponentMapping)>
 {
-    info!("Looking to match format {:?}", format);
+    //info!("Looking to match format {:?}", format);
     match format {
         D3DFormat::A8B8G8R8 => Some((Format::R8G8B8A8_sRGB, ComponentMapping::identity())),
         D3DFormat::G16R16 => None,
